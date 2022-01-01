00ce0e2ce043b96f1fe515ceae63950c648570d3
Devendra <patildevendra07072002@gmail.com>
2022-01-01 10:14:44 +0530

    Adding authentication with facebook

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 //passport-local is one of the dependencies of passport-local-mongoose
 
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
@@ -43,6 +44,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
+    facebookId: String,
     secret: String
 });
 
@@ -80,9 +82,26 @@ passport.use(new GoogleStrategy({
     }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_APP_ID,
+    clientSecret: process.env.FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    profileFields:['email','birthday',"photos"]
+},
+    function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+
 app.get("/", function (req, res) {
     res.render("home");
 });
+
+
 
 app.get("/auth/google",
     passport.authenticate("google", { scope: ["profile"] }));
@@ -96,6 +115,17 @@ app.get("/auth/google/secrets",
     });
 
 
+
+
+app.get('/auth/facebook',
+    passport.authenticate('facebook', { scope : ['email',"user_birthday","user_photos" ] }));
+
+app.get('/auth/facebook/secrets',
+    passport.authenticate('facebook', { failureRedirect: '/login' }),
+    function (req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/secrets');
+    });
 
 app.get("/submit", function (req, res) {
     if (req.isAuthenticated()) {
